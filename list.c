a68622abfd1daa2eae46e30f7aa3271457121435
Your Name <you@example.com>
2024-03-29 17:20:19 +0000

    new try

--- list.c
+++ list.c
@@ -87,7 +87,8 @@ void pushFront(List * list, void * data)
 }
 
 
-void pushBack(List * list, void * data) {
+void pushBack(List * list, void * data) 
+{
     list->current = list->tail;
     pushCurrent(list,data);
 }
@@ -95,22 +96,27 @@ void pushBack(List * list, void * data) {
 void pushCurrent(List * list, void * data) {
 }
 
-void * popFront(List * list) {
+void * popFront(List * list) 
+{
     list->current = list->head;
     return popCurrent(list);
 }
 
-void * popBack(List * list) {
+void * popBack(List * list)
+{
     list->current = list->tail;
     return popCurrent(list);
 }
 
-void * popCurrent(List * list) {
+void * popCurrent(List * list) 
+{
     return NULL;
 }
 
-void cleanList(List * list) {
-    while (list->head != NULL) {
+void cleanList(List * list) 
+{
+    while (list->head != NULL) 
+    {
         popFront(list);
     }
 }
